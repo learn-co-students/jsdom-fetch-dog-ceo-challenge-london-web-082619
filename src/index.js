280e543709f7e4f5d8cb99e637912be5bffdc767
github username <github email address>
2019-10-10 16:04:28 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,44 @@
 console.log('%c HI', 'color: firebrick')
 
-//Challnge 1
-
-
+//Challnge 1 
 function fetchDogs() {
-    debugger;
-    return fetch("https://dog.ceo/api/breeds/image/random/4")
+    // debugger;
+    return fetch("https://dog.ceo/api/breeds/image/random/4") // data -> message:[array]
     .then(resp => resp.json())
     .then(jsonImages => renderDogs(jsonImages));
 };
 
+//Challenge 1
 function renderDogs(jsonImages){
-    const main = document.querySelector("#dog-image-container");
-    jsonImages["message"].forEach(jsonImage => {
-     const img = document.createElement("img")  //to create image tag
+    const main = document.querySelector("#dog-image-container"); //tergetを定める
+    jsonImages["message"].forEach(jsonImage => { //挿入開始
+     const img = document.createElement("img")  //挿入先のタグを作る
      img.src = jsonImage // image tag にimage を挿入
-     main.appendChild(img) //
+     main.appendChild(img) //mainの下に子nodeを作ってそこに挿入
     })
 }
 
+// //Challenge 2
+// function fetchDogBreeds(){
+//  debugger;
+//  return fetch("https://dog.ceo/api/breeds/list/all") //data -> message: {[array]}
+//  .then(resp => resp.json())
+//  .then (jsonBreeds => renderDogBreeds(jsonBreeds));
+// };
+
+// //Challnge 2
+// function renderDogBreeds(jsonBreeds){
+//     const main2 = document.querySelector("#dog-breeds"); // in an ul. ul must be used with liS
+//     for (const key in jsonBreeds["message"]){ //挿入開始
+//         const li = document.createElement("li") //ulはliが必要
+//         li.innerHTML = key
+//         main2.appendChild(li)
+//     }
+// }
+
+
+
 document.addEventListener("DOMContentLoaded", function(){
-    fetchDogs();
+    fetchDogs(); //Challenge 1
+    fetchDogBreeds(); //Challenge 2 and Challenge 3
 });
