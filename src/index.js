b7909b708334484c0bb117ccd22cda0a63d42d82
github username <github email address>
2019-10-10 11:54:31 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 console.log('%c HI', 'color: firebrick')
+
+//Challnge 1
+
+
+function fetchDogs() {
+    debugger;
+    return fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(resp => resp.json())
+    .then(jsonImages => renderDogs(jsonImages));
+};
+
+function renderDogs(jsonImages){
+    const main = document.querySelector("#dog-image-container");
+    jsonImages["message"].forEach(jsonImage => {
+     const img = document.createElement("img")  //to create image tag
+     img.src = jsonImage // image tag にimage を挿入
+     main.appendChild(img) //
+    })
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    fetchDogs();
+});
